f8feef897270c09cb8d174a6492be3d8c3c0b73e
Greg Willard (r3pwn) <wickett06@gmail.com>
2021-03-06 17:03:26 -0500

    TreeViewer: add Legend

--- Scripts/SitecoreTreeViewer.ps1
+++ Scripts/SitecoreTreeViewer.ps1
@@ -15,7 +15,7 @@ function Show-ResultsDialog {
 	$legend = "This graph represents desired dependency determined based on item's id/path dependencies between each module."
 	$result2 = Read-Variable -Parameters `
  		@{ Name = "Info0"; Title = ""; Value = '<img src="data:image/svg+xml;base64,' + $image64 + '""/>'; editor = "info" },`
- 		@{ Name = "Info1"; Title = "Legend:"; Value = $legend; editor = "info" } `
+		@{ Name = "Info1"; Title = "Legend:"; Value = '<div><div style="height: 50px; width: 50px; background-color: #FFA500;">&nbsp;</div><div style="height: 50px; width: 400px;">Not Published</div></div>'; editor = "info" } `
  		-Description $description -Title "Dependency Graph" -Width 1000 -Height 800 `
  		-CancelButtonName "Exit"
 	if ($result2 -eq "cancel") {
@@ -29,6 +29,8 @@ function ProcessChildren($parent)
     $kids = $parent.children
     foreach($child in $kids)
     {
+		# everything that we check on the children, we have to check on the parent, too, 
+		# to ensure a proper heirarchy in the diagram
         $isPublished = IsPublished($child.Paths.FullPath)
         $bgColor = "white"
         if ($isPublished -ne $true) {
